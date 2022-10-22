0affb294a67e387f71890be4637cef5905c81bc7
Makoto Ohnami <z.ohnami@gmail.com>
2022-10-22 21:41:31 +0900

    streams

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,4 +1,11 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
+    def subscribed
+      puts 'subscribed!!!'
+    end
+
+    def unsubscribed
+      puts 'unsubscribed!!!'
+    end
   end
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,9 +3,10 @@ class VotesController < ApplicationController
     subject_choice = SubjectChoice.find(vote_params[:subject_choice_id])
     @vote = Vote.find_or_initialize_by(session_id: session[:session_id], subject_id: subject_choice.subject_id)
     @vote.subject_choice_id = subject_choice.id
-    @vote.save
-
-    redirect_to subject_url(subject_choice.subject_id)
+    if @vote.save
+      subject = Subject.find(subject_choice.subject_id)
+      @vote.broadcast_replace_to subject, target: 'voting_result', partial: 'subjects/voting_result', locals: { subject: subject }
+    end
   end
 
   private
